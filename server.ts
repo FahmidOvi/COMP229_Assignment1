95d999b939ef4b5f1fc2392a39330820fd0c9904
Fahmid Ovi <fahmidovi9@gmail.com>
2022-05-31 21:57:48 -0400

    Added tsconfig file and added dependencies in package.json file

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('fahmidovi-comp229-assignment1:server');
 var http = require('http');
 
