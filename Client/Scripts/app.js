61438d98f8a85812d85b5ca4dc0fda38f66ea130
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-02 18:27:33 -0400

    Edited contact file

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,14 +1,2 @@
 "use strict";
-function submitForm() {
-    console.log("hello");
-    let formData = {
-        firstName: $('#first').val(),
-        lastName: $('#last').val(),
-        email: $('#email').val(),
-        phoneNumber: $('#phone').val(),
-        message: $('#message').val()
-    };
-    console.log(formData);
-}
-;
 //# sourceMappingURL=app.js.map
